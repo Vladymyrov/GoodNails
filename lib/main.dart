a58a193d8928cac2f87bd2e402244587e4d4c65f
Vladymyrov <35792260+Vladymyrov@users.noreply.github.com>
2026-07-15 12:58:17 +0300

    Add Riverpod ProviderScope to application bootstrap

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import 'app.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
-  runApp(const GoodNailsApp());
+
+  runApp(
+    const ProviderScope(
+      child: GoodNailsApp(),
+    ),
+  );
 }
